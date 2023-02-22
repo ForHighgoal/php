b2f73e3dc43313c8b29f3ce7f5ae7c638e9a32ca
pkc20030112 <stonegiant112@gmail.com>
2023-02-22 13:41:31 -0500

    first commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php  
+    define("DB_NAME", 'f4rid_sql');
+    define("DB_USER", 'f4rid_farid');
+    define("DB_PASSWORD", 'Chocolat1-_@');
+    define("DB_HOST", 'localhost');
+    define("DB_PORT", '3306');
+    define("SYS_URL", 'https://karmatv.io');
+    define("DEBUGGING", false);
+    define("DEFAULT_LOCALE", 'en_us');
+    define("LICENCE_KEY", '5hERQZ4m6-3N2mN-2kBI4-4vwSl-2wKfe-121b96bdab98');
+    ?>
